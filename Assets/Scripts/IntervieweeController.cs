80b8d9d02f97689e9fa7ad9ee47678e0e0d14bbf
Kara Preston <kindredkara008@gmail.com>
2026-08-26 16:06:21 +0100

    Update IntervieweeController.cs

--- Assets/Scripts/IntervieweeController.cs
+++ Assets/Scripts/IntervieweeController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class IntervieweeController : MonoBehaviour
 {
