fa252ed1724e7266f6906ae63e6c9934d849fdda
Kara Preston <kindredkara008@gmail.com>
2026-08-26 13:28:24 +0100

    Added customizable dialogue with multiple lines

---
+++ Assets/Scripts/DialogueScriptableObject.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueData", menuName = "Data/DialogueObject", order = 0)]
+public class DialogueScriptableObject : ScriptableObject
+{
+    [SerializeField] private DialogueLine[] _dialogueLines;
+
+    public DialogueLine[] DialogueLines
+    {
+        get => _dialogueLines;
+    }
+}
+
+[Serializable]
+public struct DialogueLine
+{
+    public string Text;
+}
--- Assets/Scripts/SpeechController.cs
+++ Assets/Scripts/SpeechController.cs
@@ -1,44 +1,88 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeechController : MonoBehaviour
 {
-    private string _textToDisplay;
-
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private float _charactersPerSecond = 20f;
 
+    private DialogueScriptableObject _activeDialogue;
+    private int _dialogueIndex;
+
+    private Coroutine _displayTextCoroutine;
+
 #if UNITY_EDITOR
     [Tooltip("Debug")]
-    [SerializeField] private string _initialText;
+    [SerializeField] private DialogueScriptableObject _initialDialogue;
 #endif
 
-    public string TextToDisplay
+    public DialogueScriptableObject ActiveDialogue
     {
-        get => _textToDisplay;
+        get => _activeDialogue;
         set
         {
-            _textToDisplay = value;
-            StartCoroutine(DisplayText());
+            _activeDialogue = value;
+            _dialogueIndex = 0;
+
+            DisplayText();
         }
     }
 
     private void Awake()
     {
 #if UNITY_EDITOR
-        TextToDisplay = _initialText;
+        ActiveDialogue = _initialDialogue;
 #endif
     }
 
-    private IEnumerator DisplayText()
+    private void OnEnable()
+    {
+        GetComponent<Button>().onClick.AddListener(AdvanceText);
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<Button>().onClick.RemoveListener(AdvanceText);
+    }
+
+    private void DisplayText()
+    {
+        if (_displayTextCoroutine != null)
+        {
+            StopCoroutine(_displayTextCoroutine);
+        }
+
+        _displayTextCoroutine = StartCoroutine(DisplayTextCoroutine());
+    }
+
+    private IEnumerator DisplayTextCoroutine()
     {
         _text.text = "";
 
-        foreach (char character in _textToDisplay)
+        var dialogueLine = _activeDialogue.DialogueLines[_dialogueIndex];
+
+        foreach (char character in dialogueLine.Text)
         {
             _text.text += character;
             yield return new WaitForSeconds(1f / _charactersPerSecond);
+        }
+
+        _displayTextCoroutine = null;
+    }
+
+    private void AdvanceText()
+    {
+        if (_displayTextCoroutine != null)
+        {
+            StopCoroutine(_displayTextCoroutine);
+            _text.text = _activeDialogue.DialogueLines[_dialogueIndex].Text;
+            _displayTextCoroutine = null;
+        } else if (_dialogueIndex < _activeDialogue.DialogueLines.Length)
+        {
+            _dialogueIndex++;
+            DisplayText();
         }
     }
 }
