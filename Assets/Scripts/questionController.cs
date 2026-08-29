08462bdae2e6e36a645a5f120fd23af7a7aa07d0
Wren-Brannum <zenon.nowakowski8@gmail.com>
2026-08-28 22:29:50 -0500

    create global constants for text to keep consistency across several components.

--- Assets/Scripts/questionController.cs
+++ Assets/Scripts/questionController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class questionController : MonoBehaviour
 {
     [SerializeField] public GameObject questionPrefab;
+    [SerializeField] private float _charactersPerSecond = Constants._charactersPerSecond;
     
     public DialogueScriptableObject questionDialogue; 
     public SpeechController activeSpeechController;
@@ -76,7 +77,20 @@ public class questionController : MonoBehaviour
         rectTransform.offsetMin = Vector2.zero; 
         rectTransform.offsetMax = Vector2.zero; 
 
-        tmpComponent.text = text;
+        StartCoroutine(DisplayTextCoroutine(tmpComponent, text));
+    }
+
+    private IEnumerator DisplayTextCoroutine(TextMeshProUGUI textComponent, string text)
+    {
+        textComponent.text = "";
+
+        foreach (char character in text)
+        {
+            textComponent.text += character;
+            yield return new WaitForSeconds(1f / _charactersPerSecond);
+        }
+
+        textComponent.text = text;
     }
 
     private void SetupButtonInteraction(GameObject prefabInstance, SpeechController speechController, DialogueScriptableObject dialogue)
